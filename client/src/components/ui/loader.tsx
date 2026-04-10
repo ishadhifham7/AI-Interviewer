a07564511c7839a6267aa1a93bb30e5853849df4
Ishadh Ifham <ishadhifham@gmail.com>
2026-04-10 22:44:54 +0530

    added a new loader

--- client/src/components/ui/loader.tsx
+++ client/src/components/ui/loader.tsx
@@ -3,142 +3,167 @@ import React from "react";
 const Loader: React.FC = () => {
   return (
     <>
-      <style>
-        {`
-          .loader {
-            --color-one: #ffbf48;
-            --color-two: #be4a1d;
-            --color-three: #ffbf4780;
-            --color-four: #bf4a1d80;
-            --color-five: #ffbf4740;
-            --time-animation: 2s;
-            --size: 1; /* You can change the size */
-            position: relative;
-            border-radius: 50%;
-            transform: scale(var(--size));
-            box-shadow:
-              0 0 25px 0 var(--color-three),
-              0 20px 50px 0 var(--color-four);
-            animation: colorize calc(var(--time-animation) * 3) ease-in-out infinite;
-            width: 100px;
-            height: 100px;
-          }
-
-          .loader::before {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100px;
-            height: 100px;
-            border-radius: 50%;
-            border-top: solid 1px var(--color-one);
-            border-bottom: solid 1px var(--color-two);
-            background: linear-gradient(180deg, var(--color-five), var(--color-four));
-            box-shadow:
-              inset 0 10px 10px 0 var(--color-three),
-              inset 0 -10px 10px 0 var(--color-four);
-          }
-
-          .loader .box {
-            width: 100px;
-            height: 100px;
-            background: linear-gradient(
-              180deg,
-              var(--color-one) 30%,
-              var(--color-two) 70%
-            );
-            mask: url(#clipping);
-            -webkit-mask: url(#clipping);
-          }
-
-          .loader svg {
-            position: absolute;
-          }
-
-          .loader svg #clipping {
-            filter: contrast(15);
-            animation: roundness calc(var(--time-animation) / 2) linear infinite;
-          }
-
-          .loader svg #clipping polygon {
-            filter: blur(7px);
-          }
-
-          .loader svg #clipping polygon:nth-child(1) {
-            transform-origin: 75% 25%;
-            transform: rotate(90deg);
-          }
-
-          .loader svg #clipping polygon:nth-child(2) {
-            transform-origin: 50% 50%;
-            animation: rotation var(--time-animation) linear infinite reverse;
-          }
-
-          .loader svg #clipping polygon:nth-child(3) {
-            transform-origin: 50% 60%;
-            animation: rotation var(--time-animation) linear infinite;
-            animation-delay: calc(var(--time-animation) / -3);
-          }
-
-          .loader svg #clipping polygon:nth-child(4) {
-            transform-origin: 40% 40%;
-            animation: rotation var(--time-animation) linear infinite reverse;
-          }
-
-          .loader svg #clipping polygon:nth-child(5) {
-            transform-origin: 40% 40%;
-            animation: rotation var(--time-animation) linear infinite reverse;
-            animation-delay: calc(var(--time-animation) / -2);
-          }
-
-          .loader svg #clipping polygon:nth-child(6) {
-            transform-origin: 60% 40%;
-            animation: rotation var(--time-animation) linear infinite;
-          }
-
-          .loader svg #clipping polygon:nth-child(7) {
-            transform-origin: 60% 40%;
-            animation: rotation var(--time-animation) linear infinite;
-            animation-delay: calc(var(--time-animation) / -1.5);
-          }
-
-          @keyframes rotation {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-          }
-
-          @keyframes roundness {
-            0%, 60%, 100% { filter: contrast(15); }
-            20%, 40% { filter: contrast(3); }
-          }
-
-          @keyframes colorize {
-            0%, 100% { filter: hue-rotate(0deg); }
-            20% { filter: hue-rotate(-30deg); }
-            40% { filter: hue-rotate(-60deg); }
-            60% { filter: hue-rotate(-90deg); }
-            80% { filter: hue-rotate(-45deg); }
-          }
-        `}
-      </style>
-
       <div className="loader">
-        <svg width="100" height="100" viewBox="0 0 100 100">
-          <defs>
-            <mask id="clipping">
-              <polygon points="0,0 100,0 100,100 0,100" fill="black" />
-              <polygon points="25,25 75,25 50,75" fill="white" />
-              <polygon points="50,25 75,75 25,75" fill="white" />
-              <polygon points="35,35 65,35 50,65" fill="white" />
-              <polygon points="35,35 65,35 50,65" fill="white" />
-              <polygon points="35,35 65,35 50,65" fill="white" />
-              <polygon points="35,35 65,35 50,65" fill="white" />
-            </mask>
-          </defs>
-        </svg>
-        <div className="box"></div>
+        <div className="circle">
+          <div className="dot"></div>
+          <div className="outline"></div>
+        </div>
+        <div className="circle">
+          <div className="dot"></div>
+          <div className="outline"></div>
+        </div>
+        <div className="circle">
+          <div className="dot"></div>
+          <div className="outline"></div>
+        </div>
+        <div className="circle">
+          <div className="dot"></div>
+          <div className="outline"></div>
+        </div>
       </div>
+
+      <style>{`
+        /* From Uiverse.io by Li-Deheng */ 
+        .loader {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          --color: hsl(0, 0%, 87%);
+          --animation: 2s ease-in-out infinite;
+        }
+
+        .loader .circle {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          position: relative;
+          width: 20px;
+          height: 20px;
+          border: solid 2px var(--color);
+          border-radius: 50%;
+          margin: 0 10px;
+          background-color: transparent;
+          animation: circle-keys var(--animation);
+        }
+
+        .loader .circle .dot {
+          position: absolute;
+          transform: translate(-50%, -50%);
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+          background-color: var(--color);
+          animation: dot-keys var(--animation);
+        }
+
+        .loader .circle .outline {
+          position: absolute;
+          transform: translate(-50%, -50%);
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          animation: outline-keys var(--animation);
+        }
+
+        .circle:nth-child(2) {
+          animation-delay: 0.3s;
+        }
+
+        .circle:nth-child(3) {
+          animation-delay: 0.6s;
+        }
+
+        .circle:nth-child(4) {
+          animation-delay: 0.9s;
+        }
+
+        .circle:nth-child(5) {
+          animation-delay: 1.2s;
+        }
+
+        .circle:nth-child(2) .dot {
+          animation-delay: 0.3s;
+        }
+
+        .circle:nth-child(3) .dot {
+          animation-delay: 0.6s;
+        }
+
+        .circle:nth-child(4) .dot {
+          animation-delay: 0.9s;
+        }
+
+        .circle:nth-child(5) .dot {
+          animation-delay: 1.2s;
+        }
+
+        .circle:nth-child(1) .outline {
+          animation-delay: 0.9s;
+        }
+
+        .circle:nth-child(2) .outline {
+          animation-delay: 1.2s;
+        }
+
+        .circle:nth-child(3) .outline {
+          animation-delay: 1.5s;
+        }
+
+        .circle:nth-child(4) .outline {
+          animation-delay: 1.8s;
+        }
+
+        .circle:nth-child(5) .outline {
+          animation-delay: 2.1s;
+        }
+
+        @keyframes circle-keys {
+          0% {
+            transform: scale(1);
+            opacity: 1;
+          }
+
+          50% {
+            transform: scale(1.5);
+            opacity: 0.5;
+          }
+
+          100% {
+            transform: scale(1);
+            opacity: 1;
+          }
+        }
+
+        @keyframes dot-keys {
+          0% {
+            transform: scale(1);
+          }
+
+          50% {
+            transform: scale(0);
+          }
+
+          100% {
+            transform: scale(1);
+          }
+        }
+
+        @keyframes outline-keys {
+          0% {
+            transform: scale(0);
+            outline: solid 20px var(--color);
+            outline-offset: 0;
+            opacity: 1;
+          }
+
+          100% {
+            transform: scale(1);
+            outline: solid 0 transparent;
+            outline-offset: 20px;
+            opacity: 0;
+          }
+        }
+      `}</style>
     </>
   );
 };
